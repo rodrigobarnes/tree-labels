76b578fc678c002dd7ae99efd4a45660419ec6de
Rodrigo Barnes <rodrigobarnes@mac.com>
2023-03-12 18:48:45 +0000

    Improved comments

--- makepdf.sh
+++ makepdf.sh
@@ -8,11 +8,11 @@ rm input/*.svg
 rm input/*.pdf
 mv ~/Downloads/*.svg input
 
-# Convert to
+# Convert SVG to pdf
 mkdir -p output
 for f in `ls ./input/*.svg`; do 
     echo "Procesing $f"
     rsvg-convert -f pdf -o ./input/`basename $f`.pdf $f
 done
-
+# Put all the pdfs together
 pdfunite ./input/*.pdf ./output/trees-$ts.pdf
